9160fac320710d06f3e26c2acd661570ca01746c
LucyOkita <43619910+LucyOkita@users.noreply.github.com>
2024-04-24 22:43:02 -0500

    Delete ObjectDetection/Object_Detection_Algorithms/RetinaNet_Visdrone directory

--- ObjectDetection/Object_Detection_Algorithms/RetinaNet_Visdrone/eval_visdrone.sh
+++
@@ -1,5 +0,0 @@
-DetJSON=$1
-
-python visdrone/json_to_txt.py --out .visdrone_det_txt --gt-json data/visdrone/coco_format/annotations/val_label.json --det-json $DetJSON
-python visdrone_eval/evaluate.py --dataset-dir data/visdrone/VisDrone2019-DET-val --res-dir .visdrone_det_txt
-rm -rf .visdrone_det_txt
